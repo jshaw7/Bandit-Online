78b211e7c0fcec303edddc0c968f5e36f14c1c53
sarahmsachs <sarah@sarahs-mbp-5.devices.brown.edu>
2015-12-11 16:33:08 -0500

    fixed zta in hybridlinucb

--- HybridLinUCB.java
+++ HybridLinUCB.java
@@ -14,6 +14,7 @@ public class HybridLinUCB implements BanditAlgorithm {
 	HashMap<String, RealMatrix> BMap;
 	RealMatrix A0;
 	RealMatrix b0;
+	RealMatrix BetaHat;
 	double alpha;
 
 	public HybridLinUCB(double alpha) {
@@ -23,11 +24,12 @@ public class HybridLinUCB implements BanditAlgorithm {
 		BMap = new HashMap<String, RealMatrix>();
 
 		// Need to double check that it is 6 long
-		double[] zeroArrayKLong = new double[6];
+		double[] zeroArrayKLong = new double[36];
 		Double zero = new Double(0);
 		Arrays.fill(zeroArrayKLong, zero);
-		A0 = MatrixUtils.createRealIdentityMatrix(6);
+		A0 = MatrixUtils.createRealIdentityMatrix(36);
 		b0 = MatrixUtils.createColumnRealMatrix(zeroArrayKLong);
+		BetaHat = MatrixUtils.inverse(A0).multiply(b0);
 
 	}
 
@@ -40,12 +42,10 @@ public class HybridLinUCB implements BanditAlgorithm {
 		RealMatrix Ba;
 		RealMatrix ba;
 
-		RealMatrix BetaHat = MatrixUtils.inverse(A0).multiply(b0);
 		for (Article a : articles) {
 			String aId = a.getId();
 			if (!AMap.containsKey(aId)) {
-				Aa = MatrixUtils
-						.createRealIdentityMatrix(a.getFeatures().length);
+				Aa = MatrixUtils.createRealIdentityMatrix(6);
 				AMap.put(aId, Aa); // set as identity for now and we will update
 									// in reward
 
@@ -53,10 +53,9 @@ public class HybridLinUCB implements BanditAlgorithm {
 				ba = MatrixUtils.createColumnRealMatrix(zeros);
 				bMap.put(aId, ba);
 
-				double[][] BMapZeros = new double[a.getFeatures().length][user
-						.getFeatures().length];
-				for (double[] row: BMapZeros){
-				    Arrays.fill(row, 0.0);
+				double[][] BMapZeros = new double[6][36];
+				for (double[] row : BMapZeros) {
+					Arrays.fill(row, 0.0);
 				}
 				Ba = MatrixUtils.createRealMatrix(BMapZeros);
 				BMap.put(aId, Ba);
@@ -69,8 +68,8 @@ public class HybridLinUCB implements BanditAlgorithm {
 			// Make column vector out of features
 			RealMatrix xta = MatrixUtils
 					.createColumnRealMatrix(a.getFeatures());
-			RealMatrix zta = MatrixUtils.createColumnRealMatrix(user
-					.getFeatures());
+			RealMatrix zta = makeZta(
+					MatrixUtils.createColumnRealMatrix(user.getFeatures()), xta);
 
 			// Set up common variables
 			RealMatrix A0Inverse = MatrixUtils.inverse(A0);
@@ -85,9 +84,9 @@ public class HybridLinUCB implements BanditAlgorithm {
 			// Find sta
 			RealMatrix staMatrix = ztaTranspose.multiply(A0Inverse).multiply(
 					zta);
-			staMatrix = staMatrix.subtract(ztaTranspose.scalarMultiply(2)
-					.multiply(A0Inverse).multiply(BaTranspose)
-					.multiply(AaInverse).multiply(xta));
+			staMatrix = staMatrix.subtract(ztaTranspose.multiply(A0Inverse)
+					.multiply(BaTranspose).multiply(AaInverse).multiply(xta)
+					.scalarMultiply(2));
 			staMatrix = staMatrix.add(xtaTranspose.multiply(AaInverse)
 					.multiply(xta));
 			staMatrix = staMatrix.add(xtaTranspose.multiply(AaInverse)
@@ -115,7 +114,9 @@ public class HybridLinUCB implements BanditAlgorithm {
 		String aId = a.getId();
 		// Collect Variables
 		RealMatrix xta = MatrixUtils.createColumnRealMatrix(a.getFeatures());
-		RealMatrix zta = MatrixUtils.createColumnRealMatrix(user.getFeatures());
+		RealMatrix zta = makeZta(
+				MatrixUtils.createColumnRealMatrix(user.getFeatures()), xta);
+
 		RealMatrix Aa = AMap.get(aId);
 		RealMatrix ba = bMap.get(aId);
 		RealMatrix Ba = BMap.get(aId);
@@ -137,14 +138,29 @@ public class HybridLinUCB implements BanditAlgorithm {
 			ba = ba.add(xta);
 			bMap.put(aId, ba);
 		}
-		
-		//Update A0 and b0 with the new values
+
+		// Update A0 and b0 with the new values
 		A0 = A0.add(zta.multiply(ztaTranspose)).subtract(
 				Ba.transpose().multiply(MatrixUtils.inverse(Aa).multiply(Ba)));
-		b0 = b0.subtract(Ba.transpose().multiply(MatrixUtils.inverse(Aa)).multiply(ba));
-		if(clicked){
+		b0 = b0.subtract(Ba.transpose().multiply(MatrixUtils.inverse(Aa))
+				.multiply(ba));
+		if (clicked) {
 			b0 = b0.add(zta);
 		}
+	}
+
+	public RealMatrix makeZta(RealMatrix userFeature, RealMatrix articleFeature) {
+		RealMatrix product = userFeature.multiply(articleFeature.transpose());
+		double[][] productData = product.getData();
+		double[] productVector = new double[36];
+		int count = 0;
+		for (int row = 0; row < 6; row++) {
+			for (int col = 0; col < 6; col++) {
+				productVector[count] = productData[row][col];
+				count++;
+			}
+		}
+		return MatrixUtils.createColumnRealMatrix(productVector);
 	}
 
 }
